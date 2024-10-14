9ca3d76ef8b4a161311a1baa51847f46dc97e0e7
Szabina Korbai <skorbai@student.hive.fi>
2024-10-14 11:19:10 +0300

    Added very simple regisration form

--- frontend/pokedex_app/src/components/RegistrationForm.jsx
+++ frontend/pokedex_app/src/components/RegistrationForm.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useState } from "react";
 
-const LoginForm = (props) => {
+const RegistrationForm = (props) => {
 
 	const [username, setUsername] = useState('');
 	const [user, setUser] = useState([]);
 	const [password, setPassword] = useState('');
-	const [signupUsername, setSignupUsername] = useState();
+	const [email, setEmail] = useState('')
 
 
 	const handleUsername = (event) => {
@@ -15,26 +15,35 @@ const LoginForm = (props) => {
 
 	const loginAttempt = (event) => {
 		event.preventDefault()
-		console.log(`Login attempt: ${username}, ${password}`)
+		console.log(`Registration attempt: ${username}, ${password}`)
 	}
 
 	const handlePassword = (event) => {
 		setPassword(event.target.value)
 	}
 
+	const handleEmail = (event) => {
+		setEmail(event.target.value)
+	}
+
 	return (
+		
 		<section>
+			<header>Don't have an account yet? Register here</header>
 			<form onSubmit={loginAttempt}>
 				<div>
-					Username: <input value={username}  onChange={handleUsername}/>
+					Email address: <input value={email}  onChange={handleEmail}/>
+				</div>
+				<div>
+					Username: <input value={username} onChange={handleUsername}/>
 				</div>
 				<div>
 					Password: <input value={password} onChange={handlePassword}/>
 				</div>
-			<button type="submit">Log in</button>
+			<button type="submit">Register</button>
 			</form>
 		</section>
 	)
 }
 
-export default LoginForm;
+export default RegistrationForm;
--- frontend/pokedex_app/src/pages/login.jsx
+++ frontend/pokedex_app/src/pages/login.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { useState } from "react";
 import LoginForm from "../components/LoginForm.jsx"
+import RegistrationForm from "../components/RegistrationForm.jsx"
 
 const Login = () => {
-
-	const [signupUsername, setSignupUsername] = useState();
 
 	return (
 	<main>
 		<header>Login</header>
 		<LoginForm />
-		<header>Don't have an account yet?</header>
-		<p>Fill out this form to register</p>
+		<RegistrationForm  />
 	</main>
 	)
 }
