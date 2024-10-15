11428908a1dfa5562bea991bdb51174b55aa2144
Leonor Tu <jingyi.tu@outlook.com>
2024-10-15 15:13:48 +0300

    Fix: make navbar to transparent and smaller fonts

--- frontend/pokedex_app/src/components/Navbar/Navbar.jsx
+++ frontend/pokedex_app/src/components/Navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import {NavLink} from "react-router-dom"
 const Navbar = () => {
 	return (
 		<>
-			<nav className="h-24 bg-rose-900 text-center">
-				<div className="font-pokemon text-zinc-700 text-6xl py-5 flex justify-evenly">
-					<NavLink to="/Profile">
-						My Profile
-					</NavLink>
+			<nav className="h-16 bg-transparent text-center">
+				<div className="font-sans text-zinc-700 text-xl py-2 flex justify-evenly">
 					<NavLink to="/">
 						Pokedex
+					</NavLink>
+					<NavLink to="/Profile">
+						My Profile
 					</NavLink>
 					<NavLink to="/Login">
 						Login | Signup
