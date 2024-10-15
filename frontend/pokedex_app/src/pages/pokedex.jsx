b119e056db87efe747a96b29f4f4d409a01ba1f2
Leonor Tu <jingyi.tu@outlook.com>
2024-10-15 13:45:45 +0300

    Feat: add  loading button style and button disappear when there's nothing to load

--- frontend/pokedex_app/src/pages/pokedex.jsx
+++ frontend/pokedex_app/src/pages/pokedex.jsx
@@ -6,31 +6,35 @@ const Pokedex = () => {
 	const [pokemonInfo, setPokemonInfo] = useState(null);
 	// const [searchTerm, setSearchTerm] = useState("");
 	// const [selectedPokemon, setSelectedPokemon] = useState(null);
-  const [offset, setOffset] = useState(0);
-  const [loading, setLoading] = useState(false);
+	const [offset, setOffset] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const [MorePokemon, setMorePokemon] = useState(true);
 
-  const limit = 20; // Number of Pokémon per page
+	const limit = 40; // Number of Pokémon per page
 
-  const fetchPokemonList = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(`/api/pokemon?limit=${limit}&offset=${offset}`);
-      const newPokemonList = await response.json();
-      setPokemonList(prevList => [...prevList, ...newPokemonList]);
-    } catch (error) {
-      console.error("Error fetching Pokemon:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-  
-  const loadMorePokemon = () => {
-    setOffset(prevOffset => prevOffset + limit);
-  };
-  
-  useEffect(() => {
-    fetchPokemonList();
-  }, [offset]);
+	const fetchPokemonList = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch(`/api/pokemon?limit=${limit}&offset=${offset}`);
+			const newPokemonList = await response.json();
+			if (newPokemonList.length < limit) {
+				setMorePokemon(false);
+			}
+			setPokemonList(prevList => [...prevList, ...newPokemonList]);
+		} catch (error) {
+			console.error("Error fetching Pokemon:", error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const loadMorePokemon = () => {
+		setOffset(prevOffset => prevOffset + limit);
+	};
+
+	useEffect(() => {
+		fetchPokemonList();
+	}, [offset]);
 
 	return (
 	<div>
@@ -49,14 +53,15 @@ const Pokedex = () => {
 				))}
 			</ul>
 		</main>
-    <button
-      onClick={loadMorePokemon}
-      disabled={loading}
-      style={{ display: 'block', margin: '50px auto' }}
-    >
-      {loading ? "Loading..." : "Load More Pokémon"}
-    </button>
-  </div>
+		{MorePokemon && (<button
+			onClick={loadMorePokemon}
+			disabled={loading}
+			className={`block mx-auto my-12 px-6 py-3 font-semibold text-white rounded-lg shadow-lg transition-all
+				${loading ? 'bg-gray-500 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600 active:bg-blue-700'}`}
+		>
+			{loading ? "Loading..." : "Load More Pokémon"}
+		</button>)}
+	</div>
 	)
 }
 
