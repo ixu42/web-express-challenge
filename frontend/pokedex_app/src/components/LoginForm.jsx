77004200f91c3c944cceb5f07289ba80d649408c
Szabina Korbai <skorbai@student.hive.fi>
2024-10-15 13:47:03 +0300

    labels are now actually linked to their corresponding input fields

--- frontend/pokedex_app/src/components/LoginForm.jsx
+++ frontend/pokedex_app/src/components/LoginForm.jsx
@@ -7,6 +7,7 @@ const LoginForm = (props) => {
 	const [user, setUser] = useState([]);
 	const [password, setPassword] = useState('');
 	const [signupUsername, setSignupUsername] = useState();
+	//const [pwordVisibility, setPwordVisibility] = useState(false);
 
 
 	const handleUsername = (event) => {
@@ -28,11 +29,11 @@ const LoginForm = (props) => {
 			<form onSubmit={loginAttempt}>
 				<div>
 					<label className="block mb-2 text-black" htmlFor="username">Username:</label>
-					<input autoComplete="on" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" value={username}  onChange={handleUsername}/>
+					<input id="username" autoComplete="on" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" value={username}  onChange={handleUsername}/>
 				</div>
 				<div>
 					<label className="block mb-2 text-black" htmlFor="password">Password:</label>
-					<input autoComplete="on" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type="password" value={password} onChange={handlePassword}/>
+					<input id="password" autoComplete="on" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type="password" value={password} onChange={handlePassword}/>
 				</div>
 			<button className="w-full text-2xl font-pokemon bg-pink-700 hover:bg-pink-950 text-white font-bold py-2 px-4 mb-6 rounded" type="submit">Log in</button>
 			</form>
--- frontend/pokedex_app/src/components/RegistrationForm.jsx
+++ frontend/pokedex_app/src/components/RegistrationForm.jsx
@@ -32,16 +32,16 @@ const RegistrationForm = (props) => {
 			<header className="pt-4 font-pokemon w-full text-center font-bold text-pink-950 text-2xl">Don't have an account yet?<br/>Register here</header>
 			<form onSubmit={loginAttempt}>
 				<div>
-					<label className="block mb-2 text-black" htmlFor="email">Email address:</label>
-					<input className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type="email" value={email}  onChange={handleEmail}/>
+					<label className="block mb-2 text-black" htmlFor="register-email">Email address:</label>
+					<input id="register-email" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type="email" value={email}  onChange={handleEmail}/>
 				</div>
 				<div>
-					<label className="block mb-2 text-black" htmlFor="username">Username:</label>
-					<input className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" value={username} onChange={handleUsername}/>
+					<label className="block mb-2 text-black" htmlFor="register-username">Username:</label>
+					<input id="register-username" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" value={username} onChange={handleUsername}/>
 				</div>
 				<div>
-					<label className="block mb-2 text-black" htmlFor="password">Password:</label>
-					<input className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type="password" value={password} onChange={handlePassword}/>
+					<label className="block mb-2 text-black" htmlFor="register-password">Password:</label>
+					<input id="register-password" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type="password" value={password} onChange={handlePassword}/>
 				</div>
 			<button className="font-pokemon w-full text-2xl bg-pink-700 hover:bg-pink-950 text-white font-bold py-2 px-4 mb-6 rounded" type="submit">Register</button>
 			</form>
