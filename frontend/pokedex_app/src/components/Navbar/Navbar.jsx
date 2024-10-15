92d6917d9aee93495af97333f550f8929857328c
Szabina Korbai <skorbai@student.hive.fi>
2024-10-15 12:34:40 +0300

    Fixed conflict

--- frontend/pokedex_app/src/components/Navbar/Navbar.jsx
+++ frontend/pokedex_app/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,6 @@ import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
 	return (
-<<<<<<< HEAD
 		<>
 			<nav className="h-24 bg-rose-900 text-center">
 				<div className="font-pokemon text-zinc-700 text-6xl py-5 flex justify-evenly">
@@ -15,19 +14,6 @@ const Navbar = () => {
 					</NavLink>
 					<NavLink to="/Login">
 						Login | Signup
-=======
-		<div>
-			<Nav>
-				<NavMenu>
-					<NavLink to="/">
-						Pokédex
-					</NavLink>
-					<NavLink to="/Profile">
-						My Profile
-					</NavLink>
-					<NavLink to="/Login">
-						Log in / Sign-up
->>>>>>> 485e7caab21446c261e3b4416cdf03ea3f4d6131
 					</NavLink>
 				</div>
 			</nav>
