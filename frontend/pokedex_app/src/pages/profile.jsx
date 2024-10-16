15911f83949ac8701c3dba708d8f46a6e3d11017
Szabina Korbai <skorbai@student.hive.fi>
2024-10-16 11:32:50 +0300

    Started adding editing functionality to user profile

--- frontend/pokedex_app/src/pages/profile.jsx
+++ frontend/pokedex_app/src/pages/profile.jsx
@@ -10,22 +10,33 @@ const Profile = () => {
 
     const handleBio = () => {
 
+        console.log("Handling bio")
+
         if (editingBio === false)
             return (<p className="m-3 text-pink-950">{testData.bio}</p>)
         else
         {
             return (
-                <input type="text"/>
+                <section>
+                    <input type="text"/>
+                </section>
             )
         }
     }
 
+    const handleBioEditing = (event) => {
+        setEditingBio(true);
+    }
+
+    const handleBioChanging = (event) => {
+        
+    }
 
 	return (
 	<div>
         <main className="h-screen bg-cover bg-center" style={{backgroundImage: `url(${background})`}}>
-            <div className="bg-slate-500 opacity-80 pb-10 mb-10">
-            <h1 className="font-bold w-full max-w-md m-auto rounded-lg opacity-80 border-8 bg-sky-700 text-center text-5xl font-pokemon py-10 text-pink-950 border-pink-950">{testData.username}</h1>
+            <div className="bg-slate-500 opacity-80 pb-10 pt-10 mb-10">
+            <h1 className="font-bold w-full max-w-md mx-auto rounded-lg opacity-80 border-8 bg-sky-700 text-center text-5xl font-pokemon py-10 text-pink-950 border-pink-950">{testData.username}</h1>
             <div className="flex justify-evenly m-8">
             <img className="max-w-md" src={testData.profile_pic}/>
             <div className="font-bold opacity-80 rounded-lg border-double text-2xl font-mono border-pink-950 border-8 bg-sky-700">
@@ -33,8 +44,9 @@ const Profile = () => {
             </div>
             </div>
             <div className="flex m-8 justify-between">
-            <button type="button" onClick={() => setEditingBio(true)} className="bg-rose-900 hover:bg-pink-950 text-white font-bold my-3 py-2 px-4 mb-6 rounded">Change profile picture</button>
-            <button type="button" className="bg-rose-900 hover:bg-pink-950 text-white font-bold my-3 py-2 px-4 mb-6 rounded">Edit bio</button>
+            <button type="button" className="bg-rose-900 hover:bg-pink-950 text-white font-bold my-3 py-2 px-4 mb-6 rounded">Change profile picture</button>
+            <button className="bg-rose-900 hover:bg-pink-950 text-white font-bold my-3 py-2 px-4 mb-6 rounded">Save bio changes</button>
+            <button type="button" onClick={handleBioEditing} className="bg-rose-900 hover:bg-pink-950 text-white font-bold my-3 py-2 px-4 mb-6 rounded">Edit bio</button>
             </div>
             </div>
             <h1 className="font-bold w-full max-w-md m-auto rounded-lg opacity-80 border-8  bg-sky-700 text-center text-5xl font-pokemon py-6 text-pink-950 border-pink-950">Pokemon that I like:</h1>
