dd3d90f07396e0432d64676c45df766232a3e67d
Szabina Korbai <skorbai@student.hive.fi>
2024-10-16 12:51:41 +0300

    Bio editing is working now during the session. Need backend to actually store changes

--- frontend/pokedex_app/src/pages/profile.jsx
+++ frontend/pokedex_app/src/pages/profile.jsx
@@ -9,6 +9,10 @@ const Profile = () => {
     const [editingBio, setEditingBio] = useState(false);
     const [bio, setBio] = useState(testData.bio)
 
+    const updateBio = (event) => {
+        setBio(event.target.value)
+    }
+
     const handleBio = () => {
 
         console.log("Handling bio")
@@ -18,8 +22,8 @@ const Profile = () => {
         else
         {
             return (
-                <section>
-                    <textarea defaultValue={bio} maxLength={3000} rows={10} type="text"/>
+                <section className="">
+                    <textarea onChange={updateBio} className="bg-cyan-500" cols={60} defaultValue={bio} maxLength={3000} rows={10} type="text"/>
                 </section>
             )
         }
@@ -31,16 +35,17 @@ const Profile = () => {
 
     const handleBioSaving = (event) => {
         setEditingBio(false);
+        //we would send the new bio data to the server here
     }
 
 	return (
 	<div>
         <main className="h-screen bg-cover bg-center" style={{backgroundImage: `url(${background})`}}>
             <div className="bg-slate-500 opacity-80 pb-10 pt-10 mb-10">
-            <h1 className="font-bold w-full max-w-md mx-auto rounded-lg opacity-80 border-8 bg-sky-700 text-center text-5xl font-pokemon py-10 text-pink-950 border-pink-950">{testData.username}</h1>
+            <h1 className="font-bold w-full max-w-md mx-auto rounded-lg opacity-90 border-8 bg-sky-700 text-center text-5xl font-pokemon py-10 text-pink-950 border-pink-950">{testData.username}</h1>
             <div className="flex justify-evenly m-8">
             <img className="max-w-md" src={testData.profile_pic}/>
-            <div className="font-bold opacity-80 rounded-lg border-double text-2xl font-mono border-pink-950 border-8 bg-sky-700">
+            <div className="font-bold opacity-90 rounded-lg border-double text-2xl font-mono border-pink-950 border-8 bg-sky-700">
                 {handleBio()}
             </div>
             </div>
@@ -50,7 +55,7 @@ const Profile = () => {
             <button type="button" onClick={handleBioEditing} className="bg-rose-900 hover:bg-pink-950 text-white font-bold my-3 py-2 px-4 mb-6 rounded">Edit bio</button>
             </div>
             </div>
-            <h1 className="font-bold w-full max-w-md m-auto rounded-lg opacity-80 border-8  bg-sky-700 text-center text-5xl font-pokemon py-6 text-pink-950 border-pink-950">Pokemon that I like:</h1>
+            <h1 className="font-bold w-full max-w-md m-auto rounded-lg opacity-90 border-8  bg-sky-700 text-center text-5xl font-pokemon py-6 text-pink-950 border-pink-950">Pokemon that I like:</h1>
             <UserLikedPokemon likedPokemon={testData.liked_pokemons}/>
         </main>
     </div>
