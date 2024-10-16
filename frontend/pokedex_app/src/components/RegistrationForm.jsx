44d5d893d37062f1780e809e5ceed8cbeee52b9f
Szabina Korbai <skorbai@student.hive.fi>
2024-10-16 14:37:11 +0300

    Added autocomlpete attribute to input fields

--- frontend/pokedex_app/src/components/RegistrationForm.jsx
+++ frontend/pokedex_app/src/components/RegistrationForm.jsx
@@ -35,21 +35,21 @@ const RegistrationForm = (props) => {
 			<form onSubmit={loginAttempt}>
 				<div>
 					<label className="block mb-2 text-black" htmlFor="register-email">Email address:</label>
-					<input id="register-email" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type="email" value={email}  onChange={handleEmail}/>
+					<input autoComplete="off" id="register-email" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type="email" value={email}  onChange={handleEmail}/>
 				</div>
 				<div>
 					<label className="block mb-2 text-black" htmlFor="register-username">Username:</label>
-					<input id="register-username" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" value={username} onChange={handleUsername}/>
+					<input autoComplete="off" id="register-username" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" value={username} onChange={handleUsername}/>
 				</div>
 				<div>
 					<label className="block mb-2 text-black" htmlFor="register-password">Password:</label>
-					<input id="register-password" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type={
+					<input autoComplete="off" id="register-password" className="w-full p-2 mb-6 text-pink-700 border-b-2 border-pink-700 outline-none focus:bg-gray-300" type={
 						pwordVisibility	?	"text"	:	"password"
 					} value={password} onChange={handlePassword}/>
 				</div>
 				<div>
-					<label htmlFor="show-password">Show password? </label>
-					<input id="show-password" type="checkbox" value={pwordVisibility} onChange={() => setPwordVisibility((previous) => !previous)}/>
+					<label htmlFor="show-password-register">Show password? </label>
+					<input id="show-password-register" type="checkbox" value={pwordVisibility} onChange={() => setPwordVisibility((previous) => !previous)}/>
 				</div>
 			<button className="font-pokemon w-full text-2xl bg-pink-700 hover:bg-pink-950 text-white font-bold my-3 py-2 px-4 mb-6 rounded" type="submit">Register</button>
 			</form>
