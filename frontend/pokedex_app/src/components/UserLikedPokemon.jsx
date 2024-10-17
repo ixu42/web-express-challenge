bf083061dcc473ecdba12a5fed4090bc908c4192
Szabina Korbai <korbai.szabina@gmail.com>
2024-10-17 09:57:23 +0300

    The pokemon names in the list of liked pokemons on user profile can now be followed as links to pokemon profile pages

--- frontend/pokedex_app/src/components/UserLikedPokemon.jsx
+++ frontend/pokedex_app/src/components/UserLikedPokemon.jsx
@@ -10,7 +10,7 @@ const UserLikedPokemon = (props) => {
 		<ul className="grid grid-cols-10 px-4 text-center pb-3 pt-3 list-inside font-bold opacity-80 rounded-lg border-double text-2xl font-mono border-pink-950 border-4 bg-slate-800">
 			{
 				likedPokemon.map((pokemon) => {	
-					return (<li className="p-3 items-center text-white" key={pokemon}><br/>{pokemon}</li>)
+					return (<li className="p-3 items-center text-white" key={pokemon}><a href={`/pokemon/${pokemon}`}>{pokemon}</a></li>)
 				}
 			)}
 		</ul>
