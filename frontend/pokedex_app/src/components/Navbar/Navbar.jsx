738ffef5a0133c7ff83d48981ae51b565e9f4cbc
Szabina Korbai <korbai.szabina@gmail.com>
2024-10-17 09:45:00 +0300

    Made Navbar that I messed up legible again

--- frontend/pokedex_app/src/components/Navbar/Navbar.jsx
+++ frontend/pokedex_app/src/components/Navbar/Navbar.jsx
@@ -5,15 +5,12 @@ const Navbar = () => {
 	return (
 		<>
 			<nav className="h-16 bg-rose-900 text-center">
-				<div className="font-sans text-zinc-700 text-3xl py-5 flex justify-evenly">
-          <NavLink to="/Profile">
+				<div className="font-sans text-zinc-950 text-3xl py-4 flex justify-evenly">
+          			<NavLink to="/Profile">
 						My Profile
 					</NavLink>
 					<NavLink to="/">
 						Pokedex
-					</NavLink>
-					<NavLink to="/Profile">
-						My Profile
 					</NavLink>
 					<NavLink to="/Login">
 						Login | Signup
