f63b3435f2cd0dfaf18a254d985b3e2d7928539a
Szabina Korbai <skorbai@student.hive.fi>
2024-10-21 14:55:28 +0300

    Started connecting frontend to backend: communitty page renders users from database (pictures don't work yet). User profile also fetches user data from backend (again, excluding pictures)

--- frontend/pokedex_app/src/pages/profile.jsx
+++ frontend/pokedex_app/src/pages/profile.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import testData from '../assets/profile_placeholder.json';
 import background from "../assets/profile_bg.png"
 import UserLikedPokemon from "../components/UserLikedPokemon"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Profile = () => {
 
     const [editingBio, setEditingBio] = useState(false);
     const [bio, setBio] = useState(testData.bio)
+    const [ownData, setOwnData] = useState();
+
+    useEffect(() => {
+		const fetchProfile = async () => {
+			fetch("/api/profile/me")
+			.then((response) => response.json())
+			.then((data) => (setOwnData(data)))
+			.catch((error) => alert("Error fetching user list"))			
+		};
+		fetchProfile();
+	}, [])
+
+    console.log(ownData)
 
     const updateBio = (event) => {
         setBio(event.target.value)
@@ -18,7 +31,7 @@ const Profile = () => {
         console.log("Handling bio")
 
         if (editingBio === false)
-            return (<p className="m-3 text-lg">{bio}</p>)
+            return (<p className="m-3 text-lg">{ownData.bio}</p>)
         else
         {
             return (
@@ -48,7 +61,7 @@ const Profile = () => {
                                 className="rounded-md lg:w-[12rem] lg:h-[12rem] md:w-[10rem] md:h-[10rem] sm:w-[8rem] sm:h-[8rem] xs:w-[7rem] xs:h-[7rem] outline outline-2 outline-offset-2 outline-rose-900 relative lg:bottom-[5rem] sm:bottom-[4rem] xs:bottom-[3rem]" />
                         <h1
                             className="w-full text-left my-4 sm:mx-4 xs:pl-4 text-gray-800 dark:text-white lg:text-4xl md:text-3xl sm:text-3xl xs:text-xl font-mono">
-                            {testData.username}</h1>
+                            {ownData.name}</h1>
                     </div>
                     <div className="xl:w-[80%] lg:w-[90%] md:w-[90%] sm:w-[92%] xs:w-[90%] mx-auto flex flex-col gap-4 items-center relative lg:-top-8 md:-top-6 sm:-top-4 xs:-top-4">
                         <div className="w-fit text-gray-700 dark:text-gray-400 text-md">{handleBio()}</div>
