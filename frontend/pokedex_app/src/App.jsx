4ee0893b375c20c6d6189b025e737634379807ba
Ivy Xu <xyan21418@gmail.com>
2024-10-22 14:59:45 +0300

    added a page for handling 404 route

--- frontend/pokedex_app/src/App.jsx
+++ frontend/pokedex_app/src/App.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar/Navbar";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Pokedex from './pages/pokedex';
-import Profile from './pages/profile';
-import Login from './pages/login';
 import './App.css';
-import PokemonProfile from './pages/pokemon_profile';
 import './index.css'
+import ErrorBoundary from './ErrorBoundary';
+import Navbar from "./components/Navbar/Navbar";
 import Footer from './components/footer';
+import Pokedex from './pages/pokedex';
+import Login from './pages/login';
+import Profile from './pages/profile';
+import PokemonProfile from './pages/pokemon_profile';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
-import ErrorBoundary from './ErrorBoundary';
 import Community from "./components/Community";
+import PageNotFound from './pages/pageNotFound';
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             <Route path="/terms" element={<TermsOfService />} />
             <Route path="/Community" element={<Community/>}/>
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
         </main>
         <Footer />
