06e0768f1a6db0affcae5d8e0a2652901709deee
Ivy Xu <xyan21418@gmail.com>
2024-10-24 13:01:51 +0300

    centralized the buttons

--- frontend/pokedex_app/src/components/PokedexUtils/SortOptions.jsx
+++ frontend/pokedex_app/src/components/PokedexUtils/SortOptions.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SortOptions = ({ sortOrder, onSort }) => (
-  <div className="w-full md:w-1/3 flex items-center justify-end">
+  <div>
     <label className="text-lg font-semibold mr-2 text-gray-700">Sort by:</label>
     <select
       id="sortOrder"
